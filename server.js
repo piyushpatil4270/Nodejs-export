1a0ed6490e287fbfc9963ecff52a1a60f1ecd83c
piyushpatil4270 <piyushpatil4270@gmail.com>
2024-06-27 21:10:44 +0530

    added a bodyparser

--- server.js
+++ server.js
@@ -1,18 +1,24 @@
 const http = require("http");
 const express=require("express")
+const bodyParser=require("body-parser")
 
 const app=express()
 
-app.use((req,res,next)=>{
-    console.log("This is a middleware")
-    next()
-})
-app.use((req,res,next)=>{
-    console.log("In the another middleware")
-    next()
+
+app.use(bodyParser.urlencoded())
+
+app.use('/add-product',(req,res,next)=>{
+    res.send("<form action='/product' method='POST'><input type='text' name='title'><input type='text' name='size'><button type'submit' >Submit</button></form>")
 })
 
 
+app.use('/product',(req,res,next)=>{
+    console.log(req.body)
+    res.redirect("/")
+})
+app.use("/",(req,res,next)=>{
+    res.send("<h1>Hello from Node-js</h1>")
+})
 const server = http.createServer(app);
 
 server.listen(5500, () => console.log("Server started on port 5500"));
