93705d81f238357847697f756c5bedc5ba74d991
piyushpatil4270 <piyushpatil4270@gmail.com>
2024-06-28 22:40:16 +0530

    new

--- server.js
+++ server.js
@@ -1,23 +1,53 @@
 const http = require("http");
-const express=require("express")
-const bodyParser=require("body-parser")
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const app=express()
+
+const app = express();
 const server = http.createServer(app);
+let list=[]
 
-const adminRoutes=require("./routes/admin")
-const shopRoutes=require("./routes/shop")
+app.use(bodyParser.urlencoded({extended:true}));
 
 
-app.use(bodyParser.urlencoded())
-app.use("/admin",adminRoutes)
-app.use("/shop",shopRoutes)
-app.use((req,res,next)=>{
+
+app.get("/login",(req,res)=>{
   
-    res.status(404).send('<h4>Page Not Found</h4>')
+ 
+res.send(`
+  <form onsubmit='localStorage.setItem("username", document.getElementById("username").value)' action="/product" method="POST">
+    <input id="username" type="text" name="title">
+    <button type="submit">Login</button>
+  </form>
+`);
+
 })
+app.post("/product", (req, res, next) => {
+ 
+  res.redirect("/")
+});
 
+app.get("/", (req, res, next) => {
+  const htmlString = `${list.length === 0 ? '<span>No Messages</span>' : ''}
+    ${list.map((message) => `<div>${message.username} : ${message.message}</div>`)}`;
+const response=`<form onsubmit="document.getElementById('username').value=localStorage.getItem('username')" action="/" method="POST">
 
+<input id="message" type="text" name="title">
+<input id="username"  type="hidden" name="username">
+<button type="submit">Send Message</button>
 
+</form>`
+const final=htmlString+response
+res.send(final)
+});
+
+app.post('/',(req,res,next)=>{
+console.log(req.body)
+const newitem={"username":req.body.username,"message":req.body.title}
+list.push(newitem)
+
+list.map((item)=>console.log(item))
+res.redirect("/")
+})
 
 server.listen(5500, () => console.log("Server started on port 5500"));
